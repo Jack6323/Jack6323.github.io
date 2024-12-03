6cf8e969d13ec4146176458d42d10a0c76ca765f
Jack6323 <je230603@gmail.com>
2024-12-03 18:32:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
+
 
 app = Flask(__name__)
 
